5f0dbdc92569080537c8f6fe19181a9dbd531ef2
FisherHuang-Mac <fisherhuang1986@gmail.com>
2018-03-18 11:41:15 +1100

    update syntax

--- job-dsl/pipeline-job-builder.groovy
+++ job-dsl/pipeline-job-builder.groovy
@@ -1,14 +1,14 @@
-pipelinejob ('Docker Pipeline dsl') {
+ipelinejob ('Docker Pipeline dsl') {
   definition {
     cpsScm {
       scm {
-        git('https://https://github.com/bigfishfastswimer/Udemy-Jenkins-course.git') {
+        git('https://github.com/bigfishfastswimer/Udemy-Jenkins-course.git') {
           node / gitConfigName('DSL User')
           node / gitConfigEmail('fisherhuang1986@gmail.com')
           branch('master')
         }
       }
-      scriptPath('misc/Jenkins.v3')
+      scriptPath('misc/Jenkinsfile.v3')
     }
   }
   triggers {
@@ -17,9 +17,5 @@ pipelinejob ('Docker Pipeline dsl') {
   logRotator {
     daysToKeep('2')
     numToKeep('4')
-  }
-  wrappers {
-    nodejs('nodejs') // this is the name of the NodeJS installation in
-                      // Manage Jenkins -> Configure Tools -> NodeJS Installations -> Name
   }
 }
