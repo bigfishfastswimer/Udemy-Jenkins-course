89a45cc551be3eefd2733dc1823ae9a6f327ed4c
FisherHuang-Mac <fisherhuang1986@gmail.com>
2018-03-18 21:22:09 +1100

    update

--- job-dsl/pipelinebuilder.groovy
+++ job-dsl/pipelinebuilder.groovy
@@ -5,8 +5,7 @@ pipelineJob ('Docker Pipeline dsl') {
         git ('git@github.com:bigfishfastswimer/Udemy-docker-demo.git'){ node -> //is hudson.plugin.git.Gitscm
             node / gitConfigName('DSL User')
             node / gitConfigEmail('fisherhuang1986@gmail.com')
-          }
-
+          
           remote {
               //github('bigfishfastswimer/Udemy-docker-demo', 'ssh', 'github.com')
               credentials('Jenkins-UdemyCourse-aws')
